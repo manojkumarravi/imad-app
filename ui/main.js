e5b7b1d2661263d0f5c8abd7a57b3ac390600e6f
R Manoj Kumar <thekingmanoj123@gmail.com>
2017-09-02 22:45:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,17 @@ var button=document.getElementById("counter");
 var count=0;
 button.onclick=function()
   {
-   count=count+1;
-   var span=document.getElementById("count");
-   span.innerHTML=count.toString();
+      var req=new XMLHttpRequest();
+      req.onreadystatechange=function(){
+          if(req.DONE)
+          {
+              if(req.status==200)
+              {
+                  var counter=request.responseText;
+                  var span=document.getElementById("count");
+                   span.innerHTML=count.toString();
+              }
+          }
+      };
+     req.open('GET','https://imad.hasura.io/counter',true);
   };
