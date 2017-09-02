9cd897bd2e7ec081f882c6376f67935978ab0efe
R Manoj Kumar <thekingmanoj123@gmail.com>
2017-09-02 18:19:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var img=document.getElementById("image");
-img.onClick=function()
+img.onclick=function()
 {
-   img.style.marginLeft='100px';
+   img.style.marginLeft='350px';
 };
